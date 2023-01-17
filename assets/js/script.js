d59688aa83c7717c1df9294365b75d8e1904337b
brenonicacio <brenonlima04@gmail.com>
2023-01-17 12:32:43 -0300

    Adicionando o JS para aparecer o menu mobile

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const menuOpener = document.querySelector('.menu-opener');
+const menuNav = document.querySelector('.menu-nav');
+
+
+menuOpener.addEventListener('click', function() {
+    menuNav.classList.toggle('open');
+});
